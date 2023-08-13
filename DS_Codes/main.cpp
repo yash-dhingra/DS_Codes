39eb16408d2e66d5d7f281a18054eb21df042248
Yash Dhingra <77784099+yash-dhingra@users.noreply.github.com>
2023-08-13 17:47:01 +0530

    Initial Commit

---
+++ DS_Codes/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  DS_Codes
+//
+//  Created by Yash Dhingra on 13/08/23.
+//
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+    cout << "Hello World!";
+    return 0;
+}
