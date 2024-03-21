45db12095a396a7c5cbd468541615eef9aa0fb68
Yash Dhingra <77784099+yash-dhingra@users.noreply.github.com>
2024-03-21 19:14:02 +0530

    pushh

---
+++ Algo_Sem_4_Lecture/Greedy/Lab/q3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#define V 5
+
+using namespace std;
+
+int min_ele(vector<int> &arr)
+{
+    int temp = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[temp] > arr[i])
+        {
+            temp = i;
+        }
+    }
+    int temp_ele = arr[temp];
+    arr[temp] = INT_MAX;
+    return temp_ele;
+}
+
+int main()
+{
+
+    vector<int> arr;
+    for (int i = 0; i < 7; i++)
+    {
+        arr.push_back(rand() % 100);
+    }
+
+    int temp_min1 = min_ele(arr);
+    int temp_min2 = min_ele(arr);
+
+    cout << "Minimum Product of Numbers are: " << temp_min1 << " X " << temp_min2 << " = " << temp_min1 * temp_min2 << endl;
+}
